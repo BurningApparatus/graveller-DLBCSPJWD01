8de46b24edc121f3e37e5e42ba48b685730cb85b
burnapp <burningapparatus@gmail.com>
2025-09-29 12:07:55 -0400

    Added colour to logging system

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,15 +1,16 @@
 
+import { styleText } from 'node:util';
 const isDev = process.env.NODE_ENV !== 'production';
 
 export function log(...args: any[]) {
     if (isDev) {
-        console.log('[DEBUG]', ...args);
+        console.log(styleText('grey', '[DEBUG]'), ...args);
     }
 }
 export function warn(...args: any[]) {
-    console.log('[WARN]', ...args);
+    console.log(styleText('yellow', '[WARN]'), ...args);
 }
 export function error(...args: any[]) {
-    console.error('[ERROR]', ...args);
+    console.log(styleText('red', '[ERROR]'), ...args);
 }
 
